fdd4a1de532445eb24bf618ceb7bee1768251478
Borghi0 <joaopborghi@gmail.com>
2025-10-26 18:09:15 -0300

    inicialização da janela de conexão

--- src/main/java/com/mycompany/utfuneral/UTFuneral.java
+++ src/main/java/com/mycompany/utfuneral/UTFuneral.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.utfuneral;
 
-/**
- *
- * @author joaop
- */
+
 public class UTFuneral {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        View.JConexaoBD.main(null);
     }
 }
