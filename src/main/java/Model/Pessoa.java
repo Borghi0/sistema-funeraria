1d03223884a5327032e87760bb98c1c3cc749ab2
Borghi0 <joaopborghi@gmail.com>
2025-10-26 18:14:27 -0300

    CPF movido de Pessoa para Usuario

--- src/main/java/Model/Pessoa.java
+++ src/main/java/Model/Pessoa.java
@@ -5,18 +5,15 @@ import java.util.Date;
 
 public abstract class Pessoa {
     private String nome;
-    private String cpf;
     private Date data_natalidade;
 
     public Pessoa(){
-        this.nome = "";
-        this.cpf = "";
+        this.nome = "";        
         this.data_natalidade = null;
     }
     
     public Pessoa(String nome, String cpf, Date data_natalidade) {
-        this.nome = nome;
-        this.cpf = cpf;
+        this.nome = nome;        
         this.data_natalidade = data_natalidade;
     }
     
@@ -25,21 +22,13 @@ public abstract class Pessoa {
         return nome;
     }
 
-    public String getCpf() {
-        return cpf;
-    }
-
     public Date getData_natalidade() {
         return data_natalidade;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+    }    
 
     public void setData_natalidade(Date data_natalidade) {
         this.data_natalidade = data_natalidade;
