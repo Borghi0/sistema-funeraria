3800256c05871df6cc722465ff8513446a532c29
ArthurHCaron <arthurhcaron@gmail.com>
2025-11-26 16:49:42 -0300

    Cria sala

--- src/main/java/Config/UTFuneral.sql
+++ src/main/java/Config/UTFuneral.sql
@@ -22,16 +22,16 @@ CREATE TABLE IF NOT EXISTS `UTFuneral`.`endereco` (
 
 CREATE TABLE IF NOT EXISTS `UTFuneral`.`plano` (
   `pla_id` INT NOT NULL AUTO_INCREMENT,
-  `pla_nome` VARCHAR(255) NULL DEFAULT NULL,
-  `pla_preco` DOUBLE NOT NULL DEFAULT '0',  
+  `pla_preco` DOUBLE NOT NULL DEFAULT '0',
+  `pla_nome` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`pla_id`));
 
 CREATE TABLE IF NOT EXISTS `UTFuneral`.`produto` (
   `pro_id` INT NOT NULL AUTO_INCREMENT,
-  `pro_nome` VARCHAR(255) NULL DEFAULT NULL,
   `pro_perecivel` TINYINT(1) NOT NULL DEFAULT '0',
   `pro_quant_estoque` INT NOT NULL DEFAULT '0',
-  `pro_preco` DOUBLE NOT NULL DEFAULT '0',  
+  `pro_preco` DOUBLE NOT NULL DEFAULT '0',
+  `pro_nome` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`pro_id`));
 
 CREATE TABLE IF NOT EXISTS `UTFuneral`.`plano_produto` (
@@ -57,31 +57,60 @@ CREATE TABLE IF NOT EXISTS `UTFuneral`.`servico` (
 
 CREATE TABLE IF NOT EXISTS `UTFuneral`.`plano_servico` (
   `pla_id` INT NOT NULL,
-  `ser_id` INT NOT NULL,
-  PRIMARY KEY (`pla_id`, `ser_id`),
+  `SER_id` INT NOT NULL,
+  PRIMARY KEY (`pla_id`, `SER_id`),
   INDEX `pla_id` (`pla_id` ASC) VISIBLE,
-  INDEX `ser_id` (`ser_id` ASC) VISIBLE,
+  INDEX `SER_id` (`SER_id` ASC) VISIBLE,
   CONSTRAINT `plano_servico_ibfk_1`
     FOREIGN KEY (`pla_id`)
     REFERENCES `UTFuneral`.`plano` (`pla_id`),
   CONSTRAINT `plano_servico_ibfk_2`
-    FOREIGN KEY (`ser_id`)
+    FOREIGN KEY (`SER_id`)
     REFERENCES `UTFuneral`.`servico` (`ser_id`));
 
-CREATE TABLE IF NOT EXISTS sala(
-				sal_numero INT NOT NULL AUTO_INCREMENT,
-                sal_capacidade INT NOT NULL,
-                PRIMARY KEY (sal_numero)
-);
+CREATE TABLE IF NOT EXISTS `UTFuneral`.`sala` (
+  `sal_numero` INT NOT NULL AUTO_INCREMENT,
+  `sal_capacidade` INT NULL DEFAULT '0',
+  PRIMARY KEY (`sal_numero`));
 
-CREATE TABLE IF NOT EXISTS velorio(
-                sal_numero INT,
-                vel_data_horario DATETIME,
-                def_id INT NOT NULL,
-                PRIMARY KEY (sal_numero, vel_data_horario),
-                CONSTRAINT fk_velorio_sala 
-					FOREIGN KEY (sal_numero) REFERENCES sala(sal_numero)
-                                        ON DELETE CASCADE,
-                CONSTRAINT fk_velorio_defunto
-					FOREIGN KEY (def_id) REFERENCES defunto(def_id)
-);
+CREATE TABLE IF NOT EXISTS `UTFuneral`.`usuario` (
+  `usu_cpf` VARCHAR(50) NOT NULL,
+  `usu_nome` VARCHAR(255) NOT NULL,
+  `usu_data_natalidade` DATE NOT NULL,
+  `usu_login` VARCHAR(100) NOT NULL,
+  `usu_senha` VARCHAR(255) NOT NULL,
+  `usu_numero_telefone` VARCHAR(50) NULL DEFAULT NULL,
+  `usu_admin` TINYINT(1) NOT NULL DEFAULT '0',
+  `end_numero` INT NULL,
+  `end_rua` VARCHAR(255) NULL,
+  `end_cep` VARCHAR(20) NULL,
+  `pla_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`usu_cpf`),
+  UNIQUE INDEX `usu_login` (`usu_login` ASC) VISIBLE,
+  INDEX `fk_usuario_plano` (`pla_id` ASC) VISIBLE,
+  INDEX `fk_usuario_endereco` (`end_numero` ASC, `end_rua` ASC, `end_cep` ASC) VISIBLE,
+  CONSTRAINT `fk_usuario_endereco`
+    FOREIGN KEY (`end_numero` , `end_rua` , `end_cep`)
+    REFERENCES `UTFuneral`.`endereco` (`end_numero` , `end_rua` , `end_cep`),
+  CONSTRAINT `fk_usuario_plano`
+    FOREIGN KEY (`pla_id`)
+    REFERENCES `UTFuneral`.`plano` (`pla_id`));
+
+CREATE TABLE IF NOT EXISTS `UTFuneral`.`velorio` (
+  `sal_numero` INT NOT NULL,
+  `vel_data_horario` DATETIME NOT NULL,
+  `def_id` INT NOT NULL,
+  PRIMARY KEY (`sal_numero`, `vel_data_horario`),
+  INDEX `fk_velorio_defunto` (`def_id` ASC) VISIBLE,
+  CONSTRAINT `fk_velorio_defunto`
+    FOREIGN KEY (`def_id`)
+    REFERENCES `UTFuneral`.`defunto` (`def_id`)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_velorio_sala`
+    FOREIGN KEY (`sal_numero`)
+    REFERENCES `UTFuneral`.`sala` (`sal_numero`) ON DELETE CASCADE);
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
